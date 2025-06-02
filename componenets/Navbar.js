364c21d7b7581de62bdd915371831657607194c9
AKM777 <aakashmandal10@gmail.com>
2025-06-03 00:27:10 +0530

    Your descriptive commit message here

---
+++ componenets/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="h-20 bg-purple-900 flex justify-between text-white px-10 items-center">
+      <div className="logo font-bold text-lg">
+        BitLinks
+      </div>
+      <ul className="flex justify-center items-center gap-8">
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/"><li>About</li></Link>
+       <Link href="/"> <li>Shorten</li></Link>
+        <Link href="/"><li>Contact Us</li></Link>
+        <li className="flex gap-3">
+          <Link href="/"><button className="bg-purple-500 rounded lg p-2 shadow-lg">Try Now</button></Link>
+          <Link href="/"><button className="bg-purple-500 rounded lg p-2 shadow-lg">GitHub</button></Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
