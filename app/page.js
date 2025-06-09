53b558fc23d85839bd29ba15b3439a95ecc2bdf1
AKM777 <aakashmandal10@gmail.com>
2025-06-10 00:59:45 +0530

    added buttons

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local"
+import Link from "next/link";
 
 const poppins = localFont({
   weight: '100 900', 
@@ -11,13 +12,25 @@ export default function Home() {
     <div className="">
       <main className="">
         <section className="grid grid-cols-2 h-[50vh] bg-purple-200">
-          <div className="flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col justify-center items-center gap-6">
             <p className={` text-4xl text-bold font-p ${poppins.className}`}>
-            The Best URL Shortner in the Market
+            The Best URL Shortner on the Internet
+            </p>
+             <div className={`flex flex-col items-center text-xl pb-5`}>
+            <p>
+                MOST SIMPLE URL SHORTNER
+            </p>
+             <p>
+              NO LOGIN REQUIRED
             </p>
             <p>
-              We are the most straight forward URL Shortner
+              NO TRACKERS
             </p>
+            </div>
+            <div className="flex gap-5 text-amber-50 font-extrabold">
+          <Link href="/"><button className="bg-purple-500 rounded lg py-4 px-6 shadow-lg">Try Now</button></Link>
+          <Link href="/"><button className="bg-purple-500 rounded lg py-4 px-6 shadow-lg">GitHub</button></Link>
+        </div>
           </div>
             
           <div className="justify-start relative ">
--- componenets/Navbar.js
+++ componenets/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="h-20 bg-purple-900 flex justify-between text-white px-10 items-center">
-      <div className="logo font-bold text-lg">
+      <div className="logo font-bold text-2xl">
         BitLinks
       </div>
       <ul className="flex justify-center items-center gap-8">
@@ -12,10 +12,10 @@ const Navbar = () => {
         <Link href="/"><li>About</li></Link>
        <Link href="/"> <li>Shorten</li></Link>
         <Link href="/"><li>Contact Us</li></Link>
-        <li className="flex gap-3">
+        <div className="flex gap-3">
           <Link href="/"><button className="bg-purple-500 rounded lg p-2 shadow-lg">Try Now</button></Link>
           <Link href="/"><button className="bg-purple-500 rounded lg p-2 shadow-lg">GitHub</button></Link>
-        </li>
+        </div>
       </ul>
     </nav>
   )
